dc07d327b0c524f97cc11988dc787088252fc78c
AdreesGujjar22 <madreesgujjar601@gmail.com>
2025-05-05 14:10:28 +0500

    Update index.js new

--- api/index.js
+++ api/index.js
@@ -1,13 +1,5 @@
 // api/index.js
 require('dotenv').config();
-const app = require('../app');
-const { sequelize } = require('../src/models');
+const app = require('../app.js');
 
-const PORT = process.env.PORT || 5000;
-
-sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
-    console.log(`Swagger running on http://localhost:${PORT}/docs`);
-  });
-});
+module.exports = app;
